df3648e1af2e1331b939f903d605578651f9055d
tiinapennala <tiina@kiskolabs.com>
2020-04-08 16:58:43 +0300

    Refactor test to use helper method

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,6 +1,12 @@
 class CompletionsController < ApplicationController
   def create
-    current_user.todos.find(params[:todo_id]).update(completed_at: DateTime.current)
+    todo.update(completed_at: DateTime.current)
     redirect_to todos_path
+  end
+
+  private
+
+  def todo
+    @todo ||= current_user.todos.find(params[:todo_id])
   end
 end
--- spec/features/user_completes_to_spec.rb
+++ spec/features/user_completes_to_spec.rb
@@ -4,12 +4,10 @@ feature "User completes todo" do
   scenario "successfully" do
     sign_in
 
-    click_on "Add a new todo"
-    fill_in "Title", with: "Buy milk"
-    click_on "Submit"
+    create_todo "Buy butter"
 
     click_on "Mark complete"
 
-    expect(page).to have_css ".todos li.completed", text: 'Buy milk'
+    expect(page).to display_completed_todo "Buy butter"
   end
 end
